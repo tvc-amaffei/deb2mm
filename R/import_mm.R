15ede18462eacacfc142c6638a35fbc0ae38d546
tvc-amaffei <amaffei@thevisualconnection.com>
2023-11-26 10:40:19 -0500

    new example file

--- R/import_mm.R
+++ R/import_mm.R
@@ -9,7 +9,7 @@
 #' # empty pov object (and add in nodes and edges later,
 #' # with other functions)
 #' # TODO: Change this to deb2_mm_example later
-#' mmm <- import_mm(gsheet = "amaffei_mm_whoifsr_03/whoifsr_itdcsv_2019")
+#' mmm <- import_mm(gsheet = "amaffei_mm_whoifsr_04/whoifsr_itdcsv_2019")
 #' mmm
 #' }
 #' @importFrom googledrive drive_find
@@ -27,7 +27,7 @@ import_mm <- function(gsheet = NULL) {
   worksheet <- names[[1]][2]
 
   # Grab the doc
-  pov_dribble <- drive_find(pattern = "amaffei_mm_whoifsr_03",
+  pov_dribble <- drive_find(pattern = spreadsheet,
                             type = drive_mime_type("spreadsheet"))
   gs_id <- as_sheets_id(pov_dribble)
 
